42f1762a8456f0634d448b0eb0d9fc6c121aaf4c
TOR968 <tor968@yahoo.com>
2021-02-21 16:48:40 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,15 +1,37 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
+const db = require('./db');
+const { v4: uuid } = require('uuid');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return db.get('contacts').value();
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async contactId => {
+  return db.get('contacts').find({ contactId }).value();
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async contactId => {
+  const [record] = db.get('contacts').remove({ contactId }).write();
+  return record;
+};
 
-const addContact = async (body) => {}
+const addContact = async body => {
+  const id = uuid();
+  const record = {
+    id,
+    ...body,
+    // ...(body.isVaccinated ? {} : { isVaccinated: false }), // свойство по умолчанию
+  };
+  db.get('contacts').push(record).write();
+  return record;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const record = db.get('contacts').find({ contactId }).assign(body).value();
+  db.write();
+  return record.contactId ? record : null;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +39,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
