b5df5dc30e6312c46eaa720f6130cef5fb2e9567
TOR968 <tor968@yahoo.com>
2021-02-21 19:19:48 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,36 +1,65 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
-const db = require('./db');
-const { v4: uuid } = require('uuid');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.join(__dirname, './contacts.json');
+
+async function readFiles(patch) {
+  try {
+    const file = await fsPromises.readFile(patch, 'utf-8');
+    const list = JSON.parse(file);
+    return list;
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function writeFiles(patch, data) {
+  try {
+    await fsPromises.writeFile(patch, JSON.stringify(data), 'utf-8');
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 const listContacts = async () => {
-  return db.get('contacts').value();
+  const contacts = await readFiles(contactsPath);
+  return contacts;
 };
 
 const getContactById = async contactId => {
-  return db.get('contacts').find({ contactId }).value();
+  const contacts = await readFiles(contactsPath);
+  return contacts.filter(el => el.id === Number(contactId));
 };
 
 const removeContact = async contactId => {
-  const [record] = db.get('contacts').remove({ contactId }).write();
-  return record;
+  const contacts = await readFiles(contactsPath);
+  const data = contacts.filter(el => el.id !== Number(contactId));
+  console.log(contactId);
+  if (data.length !== contacts.length) {
+    await writeFiles(contactsPath, data);
+    return true;
+  }
+  return false;
 };
 
 const addContact = async body => {
-  const id = uuid();
-  const record = {
-    id,
-    ...body,
-    // ...(body.isVaccinated ? {} : { isVaccinated: false }), // свойство по умолчанию
-  };
-  db.get('contacts').push(record).write();
-  return record;
+  const contacts = await readFiles(contactsPath);
+  const newContact = { id: contacts[contacts.length - 1].id + 1, ...body };
+  const data = [...contacts, newContact];
+  await writeFiles(contactsPath, data);
+  return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  const record = db.get('contacts').find({ contactId }).assign(body).value();
-  db.write();
-  return record.contactId ? record : null;
+  const contacts = await readFiles(contactsPath);
+  const data = contacts.map(el =>
+    el.id === Number(contactId) ? { ...el, ...body } : el,
+  );
+  const updateOneContact = data.find(el => el.id === Number(contactId));
+  if (updateOneContact) {
+    await writeFiles(contactsPath, data);
+    return updateOneContact;
+  }
+  return false;
 };
 
 module.exports = {
