dd2c63c0f1ea71e96d6ff9a086f8f4df18788a10
TOR968 <tor968@yahoo.com>
2021-02-21 19:35:03 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,7 +7,7 @@ const {
   addContact,
   updateContact,
 } = require('../../model');
-// const validate = require('./validation');
+const validate = require('../../validation/validation');
 
 router.get('/', async (req, res, next) => {
   try {
@@ -47,7 +47,7 @@ router.get('/:contactId', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', validate.createContact, async (req, res, next) => {
   try {
     const contacts = await addContact(req.body);
     return res.status(201).json({
@@ -83,36 +83,35 @@ router.delete('/:contactId', async (req, res, next) => {
   }
 });
 
-router.patch('/:contactId', async (req, res, next) => {
-  // if (body) {
+router.patch('/:contactId', validate.updateContact, async (req, res, next) => {
   try {
-    const contact = await updateContact(req.params.contactId, req.body);
-    if (contact) {
-      return res.json({
-        status: 'success',
-        code: 200,
-        data: {
-          contact,
-        },
-      });
+    if (req.body) {
+      const contact = await updateContact(req.params.contactId, req.body);
+      if (contact) {
+        return res.json({
+          status: 'success',
+          code: 200,
+          data: {
+            contact,
+          },
+        });
+      } else {
+        return res.status(404).json({
+          status: 'error',
+          code: 404,
+          data: 'Not found',
+        });
+      }
     } else {
-      return res.status(404).json({
+      return res.status(400).json({
         status: 'error',
-        code: 404,
-        message: 'Not found',
+        code: 400,
+        message: 'missing fields',
       });
     }
   } catch (e) {
     next(e);
   }
-  // }
-  //   else {
-  //     return res.status(400).json({
-  //       status: 'error',
-  //       code: 400,
-  //       message: 'missing fields',
-  //     });
-  //   }
 });
 
 module.exports = router;
