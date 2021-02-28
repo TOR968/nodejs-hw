c0b507738f2bc13e3e1c20dfd024e61495f83a95
TOR968 <tor968@yahoo.com>
2021-02-28 22:41:11 +0200

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,26 +1,3 @@
-// const low = require('lowdb');
-// const FileSync = require('lowdb/adapters/FileSync');
-
-// const adapter = new FileSync('./model/contacts.json');
-// const db = low(adapter);
-
-// db.defaults({ contacts: [] }).write();
-
-// module.exports = db;
-
-// const { MongoClient } = require('mongodb');
-
-// require('dotenv').config();
-
-// const uriDb = process.env.URI_DB;
-
-// const db = MongoClient.connect(uriDb, {
-//   useUnifiedTopology: true,
-//   poolSize: 5,
-// });
-
-// module.exports = db;
-
 const mongoose = require('mongoose');
 require('dotenv').config();
 const uriDb = process.env.URI_DB;
