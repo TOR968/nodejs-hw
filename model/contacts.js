9028eca0ee07a880d9dba7facd29bab3ce189636
TOR968 <tor968@yahoo.com>
2021-03-08 15:18:31 +0200

    Update contacts.js

--- model/contacts.js
+++ model/contacts.js
@@ -1,12 +1,34 @@
 const Contact = require('./schemas/contact');
 
-const listContacts = async () => {
-  const data = await Contact.find({});
-  return data;
+const listContacts = async (
+  userId,
+  { sortBy, sortByDesc, sub, page = '1', limit = '20' },
+) => {
+  const options = { owner: userId };
+  if (sub) {
+    options.subscription = { $all: [sub] };
+  }
+  const data = await Contact.paginate(options, {
+    page,
+    limit,
+    sort: {
+      ...(sortBy ? { [`${sortBy}`]: 1 } : {}),
+      ...(sortByDesc ? { [`${sortByDesc}`]: -1 } : {}),
+    },
+    populate: {
+      path: 'owner',
+      select: 'email subscription -_id',
+    },
+  });
+  const { docs: contacts, totalDocs: total } = data;
+  return { total: total.toString(), page, limit, contacts };
 };
 
-const getContactById = async contactId => {
-  const data = await Contact.findOne({ _id: contactId });
+const getContactById = async (userId, contactId) => {
+  const data = await Contact.findOne({
+    owner: userId,
+    _id: contactId,
+  }).populate({ path: 'owner', select: 'email subscription -_id' });
   return data;
 };
 
@@ -15,17 +37,20 @@ const addContact = async body => {
   return data;
 };
 
-const updateContact = async (contactId, body) => {
+const updateContact = async (userId, contactId, body) => {
   const data = await Contact.findByIdAndUpdate(
-    { _id: contactId },
+    { owner: userId, _id: contactId },
     { ...body },
     { new: true },
   );
   return data;
 };
 
-const removeContact = async contactId => {
-  const data = await Contact.findByIdAndRemove({ _id: contactId });
+const removeContact = async (userId, contactId) => {
+  const data = await Contact.findByIdAndRemove({
+    owner: userId,
+    _id: contactId,
+  });
   return data;
 };
 
