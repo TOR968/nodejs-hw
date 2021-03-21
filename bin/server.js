a842b95ac2027c36731eff78c9f700e2d49e82f5
TOR968 <tor968@yahoo.com>
2021-03-21 12:51:58 +0200

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,19 @@
 const app = require('../app');
+
 const db = require('../model/db');
+
+const createFolderIsExist = require('../helpers/create-dir');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
+  app.listen(PORT, async () => {
+    const TMP_DIR = process.env.TMP_DIR;
+    const AVATAR_URL = process.env.AVATAR_URL;
+    await createFolderIsExist(TMP_DIR);
+    await createFolderIsExist(AVATAR_URL);
+    console.log(`Server running. Please use our API on port: ${PORT}`);
   });
 }).catch(err => {
   console.log(`Server not running. Error message: ${err.message}`);
