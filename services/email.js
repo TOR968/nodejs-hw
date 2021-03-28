a56ef5401ccc987a329195ff1d245a785566c738
TOR968 <tor968@yahoo.com>
2021-03-28 18:59:37 +0300

    Update email.js

--- services/email.js
+++ services/email.js
@@ -1,26 +1,26 @@
-const Mailgen = require('mailgen')
-const sgMail = require('@sendgrid/mail')
-const config = require('../config/email.json')
+const Mailgen = require('mailgen');
+const sgMail = require('@sendgrid/mail');
+const config = require('../config/email.json');
 
-require('dotenv').config()
+require('dotenv').config();
 
 class EmailService {
-  #sender = sgMail
-  #GenerateTemplate = Mailgen
+  #sender = sgMail;
+  #GenerateTemplate = Mailgen;
   constructor(env) {
     switch (env) {
       case 'development':
-        this.link = config.dev
-        break
+        this.link = config.dev;
+        break;
       case 'stage':
-        this.link = config.stage
-        break
+        this.link = config.stage;
+        break;
       case 'production':
-        this.link = config.prod
-        break
+        this.link = config.prod;
+        break;
       default:
-        this.link = config.dev
-        break
+        this.link = config.dev;
+        break;
     }
   }
 
@@ -31,7 +31,7 @@ class EmailService {
         name: 'System Cats',
         link: this.link,
       },
-    })
+    });
     const template = {
       body: {
         name,
@@ -47,22 +47,22 @@ class EmailService {
         outro:
           "Need help, or have questions? Just reply to this email, we'd love to help.",
       },
-    }
-    return mailGenerator.generate(template)
+    };
+    return mailGenerator.generate(template);
   }
 
   async sendEmail(verifyToken, email, name) {
-    const emailBody = this.#createTemplate(verifyToken, name)
-    this.#sender.setApiKey(process.env.SENDGRID_API_KEY)
+    const emailBody = this.#createTemplate(verifyToken, name);
+    this.#sender.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: email,
-      from: 'no-reply@system-cats.com', // Use the email address or domain you verified above
+      from: 'tor968@yahoo.com', // Use the email address or domain you verified above
       subject: 'Подтверждение регистрации',
       html: emailBody,
-    }
+    };
     // ES6
-    await this.#sender.send(msg)
+    await this.#sender.send(msg);
   }
 }
 
-module.exports = EmailService
+module.exports = EmailService;
